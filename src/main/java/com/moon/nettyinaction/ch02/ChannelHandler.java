e82432e844b7a43f4f8c3c7f81ca78892d6b4732
Chanmoey <relijifa@gmail.com>
2022-05-13 18:42:51 +0800

    客户端Channel处理类。

---
+++ src/main/java/com/moon/nettyinaction/ch02/ChannelHandler.java
@@ -0,0 +1,38 @@
+package com.moon.nettyinaction.ch02;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 客户端channel处理类。
+ * @author Chanmoey
+ * @date 2022年05月13日
+ */
+@Sharable
+public class ChannelHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    /**
+     * 与服务器建立连接完成后被调用
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        System.out.println(
+                "Client received: " + msg.toString(CharsetUtil.UTF_8)
+        );
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
